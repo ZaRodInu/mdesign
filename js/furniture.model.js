62678045b3551ea68b4bc64b792ddc39283d5ca6
ZaRodInu <patsangeniy@gmail.com>
2023-05-23 15:20:11 +0500

    first commit

---
+++ js/furniture.model.js
@@ -0,0 +1,109 @@
+const categories = [
+  {
+    id: 1,
+    name: "Мягкая мебель",
+    subcategories: [
+      {
+        id: 11,
+        name: "Диваны",
+        items: [
+          {
+            id: 111,
+            name: "Archi",
+            description: "Дизайнерский диван-кровать, шагающая еврокнижка",
+            img: " https://latqvbedpllwqek.skdesign.ru/wp-content/uploads/2022/07/225189_ARC_EKH_180_2_Velutto_14--w_500.webp",
+            rate: 6,
+          },
+          {
+            id: 112,
+            name: "Miami Lux",
+            description: "Описание дивана 2",
+            img: " ",
+            rate: 4,
+          },
+          {
+            id: 113,
+            name: "Диван 2",
+            description: "Описание дивана 2",
+            img: " ",
+            rate: 2,
+          },
+        ],
+      },
+      {
+        id: 12,
+        name: "Кровати",
+        items: [
+          {
+            id: 122,
+            name: "Кровать 1",
+            description: "Описание кровати 1",
+            img: " ",
+
+            rate: 6,
+          },
+          {
+            id: 123,
+            name: "Кровать 2",
+            description: "Описание кровати 2",
+            img: " ",
+            rate: 5,
+          },
+        ],
+      },
+      // Другие подкатегории и их объекты
+    ],
+  },
+  {
+    id: 2,
+    name: "Корпусная мебель",
+    subcategories: [
+      {
+        id: 22,
+        name: "Столы",
+        items: [
+          {
+            id: 211,
+            name: "Стол 1",
+            description: "Описание стола 1",
+
+            img: " ",
+            rate: 4,
+          },
+          {
+            id: 212,
+            name: "Стол 2",
+            description: "Описание стола 2",
+
+            img: " ",
+            rate: 6,
+          },
+        ],
+      },
+      {
+        id: 23,
+        name: "Стулья",
+        items: [
+          {
+            id: 231,
+            name: "Стул 1",
+            description: "Описание стула 1",
+
+            img: " ",
+            rate: 6,
+          },
+          {
+            id: 132,
+            name: "Стул 2",
+            description: "Описание стула 2",
+
+            img: " ",
+            rate: 3,
+          },
+        ],
+      },
+      // Другие подкатегории и их объекты
+    ],
+  },
+  // Другие категории и их подкатегории
+];
